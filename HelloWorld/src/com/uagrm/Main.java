278d9dc9edf2aded4fcd7f3e2b38398a18447423
Julio César Fernández Toranzo <jfernandez@finilager.local>
2018-09-26 16:43:23 -0400

    Cargar Hello World.

---
+++ HelloWorld/src/com/uagrm/Main.java
@@ -0,0 +1,11 @@
+package com.uagrm;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        String str = "Hello World.";
+        System.out.println(str);
+    }
+}
